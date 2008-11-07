65f9d246b0a45629c4ee86c1985e239dbc023ac8
fafa1971 <fafa1971@c8f57c82-7bd5-43be-ab20-323dc4e3b654>
2008-11-07 13:17:42 +0000

    Deleted obsolete files.

--- tests/jalr.c
+++
@@ -1,8 +0,0 @@
-int main() {
-  int a = 10;   //r3
-  int b = 10;
-  int c = a+b; //r2
-//asm("jalr $6 , $2 ");	
-asm("jalr  $2 ");  
-return 0;
-}
--- tests/mthi.c
+++
@@ -1,9 +0,0 @@
-//SPOSTA IL RISULTATO DELL'ADDIZIONE CHE È SU R2 SUL REGISTRO HI
-int main() {
-  int a = 5;
-  int b = 7;
-  int c = a+b;
-asm("MTHI $2");
-
-  return 0;
-}
