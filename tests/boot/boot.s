96688fbf80d7c4ef7817e4de03e306dfc55b4717
fafa1971 <fafa1971@c8f57c82-7bd5-43be-ab20-323dc4e3b654>
2009-02-02 13:59:45 +0000

    Added $gp initialization done by linker

--- tests/boot/boot.s
+++ tests/boot/boot.s
@@ -2,7 +2,10 @@
 
 	.text
 
-	# Initialize Stack Pointer ($sp) to make the program fit into 4 KByte of memory space
+	# Global Pointer ($gp) initialization is done by the linker
+	la $gp, _gp
+	
+	# Stack Pointer ($sp) initialization makes the program fit into 4 KByte
 	addi $29,$0,0x1000
 
 	# Initialize Return Address ($ra) to jump to the "end-of-test" special address
